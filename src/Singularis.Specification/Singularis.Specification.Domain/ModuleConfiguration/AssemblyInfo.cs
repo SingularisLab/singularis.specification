6aa13963879d2afd1399350cf5c7373ab6315d8a
anatoly-kryzhanovsky <anatoly.kryzhanovsky@gmail.com>
2019-12-27 13:02:09 +0000

    Update AssemblyInfo.cs

--- src/Singularis.Specification/Singularis.Specification.Domain/ModuleConfiguration/AssemblyInfo.cs
+++ src/Singularis.Specification/Singularis.Specification.Domain/ModuleConfiguration/AssemblyInfo.cs
@@ -1,3 +1,8 @@
 using System.Runtime.CompilerServices;
+using System.Reflection;
 
 [assembly: InternalsVisibleTo("Singularis.Specification.Executor.Nhibernate")]
+
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
+[assembly: AssemblyInformationalVersion("1.0.0.1")]
