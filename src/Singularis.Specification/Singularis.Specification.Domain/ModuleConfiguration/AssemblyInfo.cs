3144b8cfe353223127c8a8202e502a7839101ee7
Anatoly Kryzhanovsky <anatoly.kryzhanovsky@gmail.com>
2020-01-13 11:42:30 +0400

    fix proj

--- src/Singularis.Specification/Singularis.Specification.Domain/ModuleConfiguration/AssemblyInfo.cs
+++ src/Singularis.Specification/Singularis.Specification.Domain/ModuleConfiguration/AssemblyInfo.cs
@@ -2,7 +2,3 @@ using System.Runtime.CompilerServices;
 using System.Reflection;
 
 [assembly: InternalsVisibleTo("Singularis.Specification.Executor.Nhibernate")]
-
-[assembly: AssemblyVersion("1.0.1.0")]
-[assembly: AssemblyFileVersion("1.0.1.0")]
-[assembly: AssemblyInformationalVersion("1.0.1.0")]
